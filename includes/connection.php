0bfe1165a32f0c8f69df3046fc1021bcbe49c4de
Hugglermatheus125 <matheushuggler125@gmail.com>
2025-10-18 15:16:23 -0300

    gfdgdf

--- includes/connection.php
+++ includes/connection.php
@@ -2,24 +2,16 @@
 
 // Configuração da conexão com o BD
 $dbHost = 'localhost';
-$dbNome = 'teste_epav4'; // TROQUE O NOME NA VERSÃO FINAL
-$dbUsuario = 'root';
-$dbSenha = ''; // mantenha vazia se o root não tiver senha
+$dbNome = 'teste_epav4';  // TROQUE O NOME NA VERSÃO FINAL
+$dbUsario = 'root';
+$dbSenha = '';
+
+// Realizando conexão com o BD 
 
 try {
-    // Força o MySQL a usar autenticação compatível com PHP (mysql_native_password)
-    $pdo = new PDO("mysql:host=$dbHost;charset=utf8;", $dbUsuario, $dbSenha);
-    $pdo->exec("ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '';");
-
-    // Agora conecta ao banco
-    $pdo = new PDO("mysql:host=$dbHost;dbname=$dbNome;charset=utf8;", $dbUsuario, $dbSenha);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo = new PDO("mysql:host=$dbHost;dbname=$dbNome;charset=utf8;", $dbUsario, $dbSenha);
 
 } catch (PDOException $error) {
-    die("Erro na conexão: " . $error->getMessage());
+    die("Error: " . $error->getMessage());
 }
-
-    //SE NAO DER CERTO ESSA JOÇA ENTAO TENTA VER SE O SEU USUARIO E SENHA SAO IGUAL
-    // SE NAO DEIXA QUE NOS RESOLVE DPS
 ?>
-
