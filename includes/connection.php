14400d6fd2a0c3e95a489c63dd38814c76dc617c
Hugglermatheus125 <matheushuggler125@gmail.com>
2025-10-18 14:50:29 -0300

    18102025

--- includes/connection.php
+++ includes/connection.php
@@ -1,17 +1,21 @@
 <?php
 
 // Configuração da conexão com o BD
-$dbHost = 'localhost';
-$dbNome = 'teste_epav4';  // TROQUE O NOME NA VERSÃO FINAL
-$dbUsario = 'root';
-$dbSenha = '';
-
-    // Realizando conexão com o BD 
+$dbHost   = 'localhost';
+$dbNome   = 'teste_epav4'; // TROQUE O NOME NA VERSÃO FINAL
+$dbUsuario = 'root';
+$dbSenha  = ''; // mantenha vazia se o root não tiver senha
 
 try {
-    $pdo = new PDO("mysql:host=$dbHost;dbname=$dbNome;charset=utf8;", $dbUsario, $dbSenha);
+    // Força o MySQL a usar autenticação compatível com PHP (mysql_native_password)
+    $pdo = new PDO("mysql:host=$dbHost;charset=utf8;", $dbUsuario, $dbSenha);
+    $pdo->exec("ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '';");
+
+    // Agora conecta ao banco
+    $pdo = new PDO("mysql:host=$dbHost;dbname=$dbNome;charset=utf8;", $dbUsuario, $dbSenha);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 } catch (PDOException $error) {
-    die("Error: " . $error->getMessage());
+    die("Erro na conexão: " . $error->getMessage());
 }
 ?>
